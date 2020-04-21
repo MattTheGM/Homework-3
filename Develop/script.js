dfbf0885ae6443818423d2fe4030f0b42c22f4b9
Matt <matthewjmcivor@gmail.com>
2020-04-21 17:57:36 -0500

    Added length check
    Disabled other confirms, but added a basic way to check the length criteria

--- Develop/script.js
+++ Develop/script.js
@@ -13,11 +13,17 @@ function writePassword() {
     var specialCharacters = ' !?#$%^&*~`+-=_./:;\<>@';
     var numberCharactrers = '0123456789';
 
-    var lowerCaseConfirm = confirm('Would you like Lowercase Characters included?');
-    var upperCaseConfirm = confirm('Would you like Uppercase Characters included?');
-    var specialConfirm = confirm('Would you like Special Characters included?');
-    var numberConfirm = confirm('Would you like Numerical Characters included?');
+    // var lowerCaseConfirm = confirm('Would you like Lowercase Characters included?');
+    // var upperCaseConfirm = confirm('Would you like Uppercase Characters included?');
+    // var specialConfirm = confirm('Would you like Special Characters included?');
+    // var numberConfirm = confirm('Would you like Numerical Characters included?');
     var passwordLength = prompt('Please pick a Password Length between 8 and 128.');
+
+    if (Number(passwordLength) >= 8 && Number(passwordLength) <= 128){
+      alert('Your password length meets the critera!');
+    } else {
+      alert('Your length does not meet the criteria! Try again!');
+    };
 
     console.log(lowerCaseConfirm);
     console.log(upperCaseConfirm);
